e304d75cc48f9658c69ed0a83226781b221ee06f
Manish kumar <manish5854758@gmail.com>
2021-03-09 22:32:02 +0530

    Add files via upload

---
+++ Dictionaries/13.important-dictionary-function.py
@@ -0,0 +1,33 @@
+d = {'banana':45,'apple':44}
+print(d.keys())
+print(d.items())
+print(d.get('apple'))
+
+
+
+
+# d ={}
+# d['banana'] = {'1990':45,'1995':40}
+# print(d)
+# print(d['banana'][1])
+
+# print(d['banana'][2])
+
+
+
+
+
+
+# d =dict()
+# print(d)
+
+# d =dict(key='values')
+# print(d)
+
+# d ={'nokia':'2000','iphone':'25465',}
+# print(d['nokia'])
+
+
+
+# d = {'key1':'value1','key2':'values2'}
+# print(d['key1'])
