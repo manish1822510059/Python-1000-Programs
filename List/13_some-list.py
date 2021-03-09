c82150878dfe167993633a2174ee5c8da6de2de2
Manish kumar <manish5854758@gmail.com>
2021-03-09 22:31:14 +0530

    initial commi

---
+++ List/13_some-list.py
@@ -0,0 +1,116 @@
+
+
+
+
+list1 = [2,7,5,64,14]
+
+# for num in list1:
+#     if num % 2==0:
+#         # print(num,end =" ")
+
+
+even_ni = list(filter(lambda x:(x %2==0),list1))
+print(even_ni)
+print(type(list1))
+        
+
+
+
+
+even  = [num for num in list1 if num%2==0]
+
+# print(even)
+
+# a = int(list1)
+# p = a/2==0
+
+# print(p)
+# print(list1(type))
+
+
+
+
+
+
+# nums = [1,1,0,1]
+# print(all(nums))
+# print(any(nums))
+
+
+
+
+# chars = ['a','b','c',12]
+# print(all(chars))
+
+
+
+
+
+# import copy as c
+a = [1,3,5,4,0,2,6]
+# a.sort(reverse = False)
+# print(a)
+
+# rev = reversed(a)
+# print(list(rev))
+# num1 = 12
+# num2 = '12'
+# print(num1 +int(num2))
+
+# rev = a[::-1]
+# print(rev)
+
+#in keyword in list
+
+
+
+# print(all(a))
+# print(1000 in a)
+
+
+
+
+# print(len(a))
+
+
+
+
+
+
+# new_list = []
+# print(new_list)
+# new_list = c.copy(a)
+# print(new_list)
+
+
+
+
+
+
+
+
+
+
+
+# a.extend([2,8,"hi"])
+# print(a.remove(1))
+# print(a.reverse())
+# print(a)
+
+# print(a.index(5))
+# a.insert(0,0)
+# print(a)
+# a.append(1)
+# print(a)
+# print(a.count(1))
+
+# a.sort(reverse=True)
+# print(a)
+
+# print(a[1:6:2])
+# del a[:2]
+# print(a)
+
+
+
+
