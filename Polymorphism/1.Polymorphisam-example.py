be29eca938094878ee7c8f43befddde4d2e2738c
Manish kumar <manish5854758@gmail.com>
2021-03-18 20:52:51 +0530

    initial commit

---
+++ Polymorphism/1.Polymorphisam-example.py
@@ -0,0 +1,34 @@
+class Duck:
+    def quack(self):
+        print("Quaaaaaaaaaaaaack")
+
+    def feathers(self):
+        print("the duck has white feathers")
+
+
+class Person:
+    def quack(self):
+        print("The person can imitate duck")
+
+    def feathers(self):
+        print("the person takes a feather from ground and takes it")
+
+    def name(self):
+        print("Matt Henry")
+
+def in_the_forest(obj):
+    obj.quack()
+    obj.feathers()
+
+donald = Duck()
+matt = Person()
+
+in_the_forest(donald)
+
+print("---------------------------")
+in_the_forest(matt)
+
+
+
+
+
